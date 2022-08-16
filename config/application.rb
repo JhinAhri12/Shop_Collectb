52c9beb7cceae644fc570aad9d3cbf8226ae231d
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-08-16 17:10:59 +0200

    add internationalisation, shop views

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module ShopCollect
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    I18n.available_locales = [:fr,:en]
+    config.i18n.default_locale = :fr
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
