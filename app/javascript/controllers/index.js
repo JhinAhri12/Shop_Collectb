d3b32d5938736f77656bc31a259aca6428f0e402
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-08-16 17:56:36 +0200

    add stimulus shop controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ShopController from "./shop_controller"
+application.register("shop", ShopController)
